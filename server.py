54e746c3e528b2620124c2068d42422400f8d928
knighan <andrew.knight@pobox.com>
2021-05-20 05:39:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -6,11 +6,13 @@ PORT = 8080
 name = os.environ['NAME']
 if name == None or len(name) == 0:
   name = "Santander"
-MESSAGE = "Hello, " + name + "!"
+
 # current date and time
 now = datetime.now()
 date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 print("date and time:",date_time)
+
+MESSAGE = date_time + " Hello, " + name + "!"
 
 print(" Message: '" + MESSAGE + "'")
 
